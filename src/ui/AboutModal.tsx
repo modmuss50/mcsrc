cb6d7d89c1b3f011b6455586322daef514f93396
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2025-12-03 22:48:37 +0100

    Thank you github

--- src/ui/AboutModal.tsx
+++ src/ui/AboutModal.tsx
@@ -35,7 +35,7 @@ const AboutModal = () => {
                 <p>NOTE! This website is not redistributing any Minecraft code or compiled bytecode. The minecraft jar is downloaded directly from Mojang's servers to your device when you use this tool. Check your browser's network requests!</p>
                 <p>The <a href="https://github.com/Vineflower/vineflower">Vineflower</a> decompiler is used after being compiled to wasm as part of the <a href="https://www.npmjs.com/package/@run-slicer/vf">@run-slicer/vf</a> project.</p>
 
-                <p><a href="https://github.com/modmuss50/mcsrc">GitHub</a></p>p>
+                <p><a href="https://github.com/modmuss50/mcsrc">GitHub</a></p>
 
                 <Eula onAccept={() => setIsModalOpen(false)} />
             </Modal>
